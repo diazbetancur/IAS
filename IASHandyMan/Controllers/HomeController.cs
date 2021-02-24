3683d6e33e3a5358988e1e1d88020f7f509be60f
diazbetancur <49962109+diazbetancur@users.noreply.github.com>
2021-02-24 11:31:24 -0500

    IAS Prueba Tecnica
    Prueba Tecnica, la DB fue realizada en MySql, no tenia un ejecutable de SQL Server y realizandolo desde la misma solucion se estaba parando la maquina, no tengo una con muy buenas caracteristicas fisicas.

---
+++ IASHandyMan/Controllers/HomeController.cs
@@ -0,0 +1,24 @@
+﻿using IASHandyMan.Conexion;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace IASHandyMan.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            //string dato = DBConexion.GetLogin(11, "HolaMundo");
+            //Login.ServiceRegisterTimeClient obj = new Login.ServiceRegisterTimeClient();
+
+            //dato  =  obj.validarEmpleado();
+
+            ViewBag.Title = "Home Page";
+
+            return View();
+        }
+    }
+}
---
+++ IASHandyMan/Controllers/LoginController.cs
@@ -0,0 +1,40 @@
+﻿using IASHandyMan.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace IASHandyMan.Controllers
+{
+    public class LoginController : Controller
+    {
+
+        private Employee employee = new Employee();
+        // GET: Login
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Login(Employee loginDataModel)
+        {
+            if (ModelState.IsValid)
+            {
+                // AQUÍ EL CÓDIGO DE VALIDACIÓN DEL USUARIO
+                //WebService.Login login = new WebService.Login();
+                //if ( login.validarEmpleado(loginDataModel ).Equals("1"))
+                //{
+
+                //}
+
+                return RedirectToAction("LoginOk");
+            }
+            else
+            {
+                return View(loginDataModel);
+            }
+        }
+    }
+}
